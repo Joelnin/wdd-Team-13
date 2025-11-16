2bea4b52951137bb89492cf5a4f8cbc5cf8bf685
Joelnin <joelninr@gmail.com>
2025-11-16 16:53:28 -0400

    Connecting to API and expand Inventory

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,3 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
-
-const products = document.querySelector(".product-list");
-
-const dataSource = new ProductData("tents");
-const productList = new ProductList("Tents", dataSource, products);
-
-productList.init();
